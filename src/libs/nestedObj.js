b44f169f190e5fa902c69127d3f8dac3f80d5845
null <null>
2024-04-07 20:56:57 +0900

    sf

--- src/libs/nestedObj.js
+++ src/libs/nestedObj.js
@@ -2,7 +2,8 @@ export default function (obj, props, value, command=false, createNesting=false) 
     if (!props) return obj;
     if (props.length === 0) return obj
     let prop = props[0]
-    for (let i = 0; i < value ? props.length-1 : props.length; i++) {
+    let maxDig = value ? props.length-1 : props.length
+    for (let i = 0; i < maxDig; i++) {
       prop = props[i]
       if (!obj[prop] && createNesting) {
         obj[prop] = {}
