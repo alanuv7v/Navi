57c89d1a5584010cb953e95cf597541e640b8e91
alanuv7v <alanuv7v@gmail.com>
2024-05-21 23:45:15 +0900

    sqljs runnin in App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,58 @@
 // This file must import components from other modules
 // and merely display and organize them to show the final output to the cilent, not creating one.
 
+
+
+//test
+
+// Dynamically import the WebAssembly module
+
+/* import sql from "./tech/sqlite/sqlite3"
+console.log(sql)
+window.sql = await sql()
+//const { default: initSqlJs } = await import("./tech/sqlite/sqlite3.wasm?init"); */
+
+import sqlJs from "sql.js"
+window.sqlJs = sqlJs
+
+const SQL = await initSqlJs(
+    {locateFile: file => `https://sql.js.org/dist/${file}`}
+)
+const db = new SQL.Database();
+window.SQL = SQL
+window.db = db
+
+/* 
+
+window.initSqlJs({
+    locateFile: filename => `https://cdnjs.cloudflare.com/ajax/libs/sql.js/1.6.2/${filename}`
+}).then(function(SQL){
+    // Create a database
+    var db = new SQL.Database();
+
+    // Create a table
+    db.run("CREATE TABLE test (col1, col2);");
+
+    // Insert two rows: (1,111) and (2,222)
+    db.run("INSERT INTO test VALUES (?,?), (?,?)", [1, 111, 2, 222]);
+
+    // Prepare an SQL statement
+    var stmt = db.prepare("SELECT * FROM test WHERE col1 BETWEEN ? AND ?");
+    stmt.bind([1, 2]);
+
+    // Fetch the results of the query
+    while (stmt.step()) {
+        var row = stmt.getAsObject();
+        console.log("Row: ", row);
+    }
+    stmt.free(); // Free the resources allocated to this statement
+}).catch(function(err){
+    console.error("Failed to initialize SQL.js:", err);
+});
+ */
+
+//test end
+
 import van from "vanjs-core"
 
 import * as UserActions from "./natural/UserActions"
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import ViteYaml from '@modyfi/vite-plugin-yaml';
+import wasm from "vite-plugin-wasm";
 
 export default defineConfig({
     //root: 'src',
@@ -11,6 +12,7 @@ export default defineConfig({
     },
     plugins: [
         ViteYaml(), 
+        wasm(),
     ],
 });
 
