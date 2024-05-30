43c344af98668123ab88763d4dbf84b0535738c7
alanuv7v <135824493+alanuv7v@users.noreply.github.com>
2024-05-30 14:36:09 +0000

    MVC model upcoming, a lotta things are changing

---
+++ src/entity/model/NodeModel.js
@@ -0,0 +1,33 @@
+import NodeData from "../static/NodeData"
+
+export default class NodeModel extends NodeData {
+
+    constructor (data) {
+        super(...data)
+        //should sync all these with the LocalDB automatically
+    }
+    
+    path () {
+        let originPath = this?.origin?.path()
+        if (originPath) return [...originPath, this.key]
+        else return [this.key]
+    }
+    
+    pathString () {
+        return this.path().join("/")
+    }
+    
+    delete() {
+        return true
+    }
+
+    changeOrigin (value) {
+        //change this.origin
+        //change previous origin's children
+        
+        return true
+    }
+
+    
+
+}
---
+++ src/entity/static/NodeData.js
@@ -0,0 +1,12 @@
+export default class NodeData  {
+    
+    constructor (id, key, value, origin, relations) {
+        
+        this.key = key
+        this.value = value
+        this.origin = origin
+        this.relations = relations
+
+    }
+
+}
