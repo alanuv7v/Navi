2b370e70371e3cfec04ed5600f4e1d03e3233e31
alanuv7v <135824493+alanuv7v@users.noreply.github.com>
2024-06-08 10:49:12 +0900

    Update NodeData.js

--- src/entity/static/NodeData.js
+++ src/entity/static/NodeData.js
@@ -2,11 +2,10 @@ import { v4 as uuidv4 } from 'uuid';
 
 export default class NodeData  {
     
-    constructor (id, value, origin, links) {
+    constructor (id, value, links, authentic) {
 
         this.id = id || uuidv4().replaceAll("-", "") //uuid
-        this.value = value //text
-        this.origin = origin //uuid reference
+        this.value = value //text. if the value starts with "%", this node is authentic, meaning that no node that shares the same value can be created.
         this.links = typeof links === "string" ? JSON.parse(links) : [] //array of [tie, node.id]. ex) ["reason", "someuuid"]
 
         this.metadata = {
