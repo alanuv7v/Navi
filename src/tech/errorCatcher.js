057efc1b63646a8ba91552174ded4cf4d2781c15
alanuv7v <135824493+alanuv7v@users.noreply.github.com>
2024-06-20 14:46:08 +0000

    safd

--- src/tech/errorCatcher.js
+++ src/tech/errorCatcher.js
@@ -10,4 +10,9 @@ export default function () {
         Logger.log(`UNHANDLED ERROR: ${error.stack}`, "error");
     };
 
+    window.addEventListener("error", function (e) {
+        Logger.log(`UNHANDLED ERROR: ${error.stack}`, "error");
+        return false;
+     })
+
 }
